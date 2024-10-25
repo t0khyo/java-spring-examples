e6599b24c82138b8f2c0470c1a7cc3fe6db65bab
t0khyo <abdullrhman.eltoukhy@gmail.com>
2024-10-25 23:53:38 +0300

    First commit 🚀

---
+++ inventory-excel-erator/src/main/java/com/t0khyo/inventoryexcelerator/InventoryExcelEratorApplication.java
@@ -0,0 +1,13 @@
+package com.t0khyo.inventoryexcelerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryExcelEratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryExcelEratorApplication.class, args);
+	}
+
+}
---
+++ inventory-excel-erator/src/test/java/com/t0khyo/inventoryexcelerator/InventoryExcelEratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t0khyo.inventoryexcelerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryExcelEratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
